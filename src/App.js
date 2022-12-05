ddfcb18aa9be7407795c4cc57dae1b167bca07ad
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-05 13:27:49 +0900

    #3. 01. header 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!!</div>;
+    return (
+      <div className="App">
+        <Subject></Subject>
+      </div>
+    );
   }
 }
 

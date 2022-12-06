eda194a46609e41b5fbed4467ede609bf217b02d
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-06 17:03:24 +0900

    #6. 02. key props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      subject: { title: 'WEB', sub: 'World Wide Web' },
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+      ],
     };
   }
   render() {
@@ -18,7 +22,7 @@ class App extends Component {
           title={this.state.subject.title}
           sub={this.state.subject.sub}
         ></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
           desc="HTML is HyperText Markup Language."

f580022c8ad0dddc4e9cdd84ddf799a9361e5a74
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-06 16:41:22 +0900

    #6. 01. state 사용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'WEB', sub: 'World Wide Web' },
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
         <TOC></TOC>
         <Content
           title="HTML"

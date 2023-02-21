7c3ec232b3c5f712bfb5572daedb59ff12a05785
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-21 10:00:05 +0900

    #9. JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+// import "./App.css";
+
+import MyHeader from "./MyHeader";
 
 function App() {
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
+  const number = 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+      <h2 style={style.h2}>안녕 리액트</h2>
+      <b style={style.bold_text} id="bold_text">
+        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
+      </b>
     </div>
   );
 }

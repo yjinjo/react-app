c9628f5a63b1b72198123507ee6c44b2f5ec9578
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-21 13:29:25 +0900

    #10. 사용자 입력 처리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 
 function App() {
   return (
     <div className="App">
-      <h2>일기장</h2>
+      <DiaryEditor />
     </div>
   );
 }

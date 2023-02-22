8dcf19ffcf4b13195abdc6ae81afe5ff8d1a66ae
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-22 10:26:07 +0900

    #10. 데이터 삭제 (수정)

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId} 가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
@@ -30,7 +30,7 @@ function App() {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onDelete={onDelete} diaryList={data} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
     </div>
   );
 }

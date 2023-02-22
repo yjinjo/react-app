9c17e8435f9fbf88450986390ad95cc21b75e90e
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-22 10:28:15 +0900

    #10. 데이터 삭제 (수정) Refactoring

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -6,6 +6,12 @@ const DiaryItem = ({
   created_date,
   emotion,
 }) => {
+  const handleRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onRemove(id);
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -16,15 +22,7 @@ const DiaryItem = ({
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
-            onRemove(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <button onClick={handleRemove}>삭제하기</button>
     </div>
   );
 };

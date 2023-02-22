a5334960b88a2fbb118046e519de9e7a9e6ff14b
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-22 18:03:16 +0900

    #10. 최적화 2 - React.memo 2

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from "react";
 
-const TextView = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`update :: Text: ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`update :: Counter: ${count}`);
+    console.log(`CounterA Update - count: ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`CounterB Update - obj: ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+  // return true // 이전 props 와 현재 props가 같다 => 리랜더링을 일으키지 않습니다.
+  // return false // 이전과 현재가 다르므로 rerendering을 일으키라는 것입니다.
+  return prevProps.obj.count === nextProps.obj.count;
+};
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
-      <h2>text</h2>
-      <TextView text={text} />
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <div>
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B button
+        </button>
+      </div>
     </div>
   );
 };

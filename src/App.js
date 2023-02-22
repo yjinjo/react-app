151772016db981169cbe5ea8d80b7b0f5d380ef1
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-22 10:22:26 +0900

    #10. 데이터 삭제

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import { useRef, useState } from "react";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "Youngjin Jo",
-//     content: "하이 1",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "홍길동",
-//     content: "하이 2",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "아무개",
-//     content: "하이 3",
-//     emotion: 1,
-//     created_date: new Date().getTime(),
-//   },
-// ];
-
 function App() {
   const [data, setData] = useState([]);
 
@@ -45,10 +21,16 @@ function App() {
     setData([newItem, ...data]);
   };
 
+  const onDelete = (targetId) => {
+    console.log(`${targetId} 가 삭제되었습니다.`);
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 }

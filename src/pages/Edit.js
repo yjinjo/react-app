9c19442223111beca45aebb0c0ddb51c9547b401
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-23 16:18:33 +0900

    #11. Query String - useSearchParms 이용하여 Query String의 값 가져오기

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,25 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode:", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이 곳은 일기 수정 페이지 입니다.</p>
+      <button
+        onClick={() => {
+          setSearchParams({ who: "Youngjin Jo" });
+        }}
+      >
+        QS 바꾸기
+      </button>
     </div>
   );
 };

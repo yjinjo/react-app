d03049f24f420d2b65e290c879a5403303602ab5
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-23 20:05:13 +0900

    #11. MyButton Component 생성

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
 
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} alt="" />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} alt="" />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />

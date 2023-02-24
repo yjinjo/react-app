98aeca14ad5a57a6394d87e6aa92b3d8f6923b49
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-24 14:43:01 +0900

    #11. 리스트- 오래된순, 최신순 정렬

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  // 정렬 기준을 저장할 state
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    // 원본 배열을 정렬하는 것이 아니라 객체를 복사해서 정렬을 합니다.
+    // 배열 -> JSON -> 다시 parse 를 이용하여 배열로 만듦
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((it) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>

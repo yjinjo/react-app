883da1037d6c2f151d3ef0de9b30e2914147e409
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-24 15:39:28 +0900

    #11. 수정하기 열 스타일링 및 content detail & edit page navigate

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,8 +1,23 @@
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -10,11 +25,13 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div className="info_wrapper">
+      <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
-      <div></div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text="수정하기" />
+      </div>
     </div>
   );
 };

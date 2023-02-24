4bfe751e22bd906beadd8d6e876a0430a63e024c
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-24 13:32:19 +0900

    #11. Header - 왼쪽/오른쪽 버튼 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 
 import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
 
 const Home = () => {
   const [curDate, setCurDate] = useState(new Date());
-  console.log(curDate);
-
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
-      <MyHeader headText={headText} />
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      />
     </div>
   );
 };

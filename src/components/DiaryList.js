34cee91b609bb5c4c56b180d09d6fee5afd43d52
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-24 14:54:26 +0900

    #11. 리스트 - 감정 필터링

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,12 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안 좋은 감정만" },
+];
+
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -21,7 +27,17 @@ const DiaryList = ({ diaryList }) => {
   // 정렬 기준을 저장할 state
   const [sortType, setSortType] = useState("latest");
 
+  // 감정 상태를 필터링할 state
+  const [filter, setFilter] = useState("all");
+
   const getProcessedDiaryList = () => {
+    const filterCallback = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
     const compare = (a, b) => {
       if (sortType === "latest") {
         return parseInt(b.date) - parseInt(a.date);
@@ -32,7 +48,10 @@ const DiaryList = ({ diaryList }) => {
     // 원본 배열을 정렬하는 것이 아니라 객체를 복사해서 정렬을 합니다.
     // 배열 -> JSON -> 다시 parse 를 이용하여 배열로 만듦
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallback(it));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
@@ -43,8 +62,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );

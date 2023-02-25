682c81a7c8427d32852ab6599f40591ba6e48e08
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-25 14:17:31 +0900

    #11. NEW 구현 - Header

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,16 @@
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { useNavigate } from "react-router-dom";
+
 const New = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>New</h1>
-      <p>이 곳은 일기 작성 페이지 입니다.</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
+      />
     </div>
   );
 };

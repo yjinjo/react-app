9fd3a941c65e3fb2f1f5884cc37d1977f7c833c5
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-25 14:42:29 +0900

    #11. NEW 구현 - Calendar 스타일링

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -14,7 +14,7 @@ const DiaryEditor = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
+    <div className="DiaryEditor">
       <MyHeader
         headText={"새 일기쓰기"}
         leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
@@ -23,9 +23,9 @@ const DiaryEditor = () => {
       <div>
         <section>
           <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
+          <div className="input_box">
             <input
-              className="input-date"
+              className="input_date"
               value={date}
               onChange={(e) => setDate(e.target.value)}
               type="date"

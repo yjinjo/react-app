c2d165435f00a49644ad510001f8fc0a068d02a7
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-25 14:30:45 +0900

    #11. NEW 구현 - Calendar

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,15 @@
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
 
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+
   const navigate = useNavigate();
 
   return (
@@ -11,6 +18,20 @@ const New = () => {
         headText={"새 일기쓰기"}
         leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
       />
+
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };

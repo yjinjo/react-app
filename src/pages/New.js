5944d71bdffd50fb6f55c546173bd5823472a9aa
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-25 14:35:55 +0900

    #11. NEW 구현 - DiaryEditor Refactoring

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,37 +1,9 @@
-import MyHeader from "../components/MyHeader";
-import MyButton from "../components/MyButton";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
+import DiaryEditor from "../components/DiaryEditor";
 
 const New = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-
-  const navigate = useNavigate();
-
   return (
     <div>
-      <MyHeader
-        headText={"새 일기쓰기"}
-        leftChild={<MyButton text="< 뒤로가기" onClick={() => navigate(-1)} />}
-      />
-
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className="input-box">
-            <input
-              className="input-date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              type="date"
-            />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
